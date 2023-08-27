68bf59bd1f140a51ce0e2e4da8083e0f8b9c89e1
Ben Hur <bhur777@gmail.com>
2023-08-27 04:14:54 -0400

    Add clerk auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,23 @@
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 const LandingPage = () => {
-	return <div>LandingPage - You are not signed in ...probably...</div>;
+	return (
+		<div>
+			<p>LandingPage - You are not signed in ...probably...</p>
+			<div>
+				<Link href="/sign-in" className="bg-black text-white">
+					Sign In
+				</Link>
+				<Link href="/sign-up" className="bg-black text-white">
+					Sign Up
+				</Link>
+			</div>
+			<div>
+				<UserButton afterSignOutUrl="/" />
+			</div>
+		</div>
+	);
 };
 
 export default LandingPage;
