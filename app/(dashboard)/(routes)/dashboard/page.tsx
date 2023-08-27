ee2360ef6aabb1b58df56b97a860d58d378267e6
Ben Hur <bhur777@gmail.com>
2023-08-27 05:34:40 -0400

    Stylize dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 const DashboardPage = () => {
-	return <div>DashboardPage</div>;
+	return (
+		<div>
+			<div className="mb-8 space-y-4">
+				<h2 className="text-2xl md:text-4xl font-bold text-center">
+					Explore the power of AI
+				</h2>
+				<p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+					Experience the power of AI - with the following:
+				</p>
+			</div>
+			<div className="px-4 md:px-20 lg:px32 space-y-4"></div>
+		</div>
+	);
 };
 
 export default DashboardPage;
