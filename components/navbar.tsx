5a325c95c069369afec92a359c17caadbff2dc3a
Ben Hur <bhur777@gmail.com>
2023-08-27 09:23:38 -0400

    Fix button nesting error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,12 +3,12 @@ import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
 	return (
-		<nav className="flex items-center p-4">
+		<div className="flex items-center p-4">
 			<MobileSidebar />
 			<div className="flex w-full justify-end">
 				<UserButton afterSignOutUrl="/" />
 			</div>
-		</nav>
+		</div>
 	);
 };
 
