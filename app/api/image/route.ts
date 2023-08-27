4d4d7c8ed177a60b63c35c626aa608ca85164adc
Ben Hur <bhur777@gmail.com>
2023-08-27 17:11:49 -0400

    create image route.ts

---
+++ app/api/image/route.ts
@@ -0,0 +1,48 @@
+import OpenAI from "openai";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
+});
+
+export async function POST(req: Request) {
+	try {
+		const { userId } = auth();
+		const body = await req.json();
+		const { prompt, amount = 1, resolution = "512x512" } = body;
+
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		if (!openai) {
+			return new NextResponse("API Key not configured properly.", {
+				status: 500,
+			});
+		}
+
+		if (!prompt) {
+			return new NextResponse("Prompt is required", { status: 400 });
+		}
+
+		if (!amount) {
+			return new NextResponse("Amount is required", { status: 400 });
+		}
+
+		if (!resolution) {
+			return new NextResponse("Resolution is required", { status: 400 });
+		}
+
+		const response = await openai.images.generate({
+			prompt,
+			n: parseInt(amount, 10),
+			size: resolution,
+		});
+		console.log(response.data);
+		return NextResponse.json(response.data);
+	} catch (error) {
+		console.log("[CONVERSATION_ERROR]", error);
+		return new NextResponse("Internal error", { status: 500 });
+	}
+}
