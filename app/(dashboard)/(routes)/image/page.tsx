64c0616228c556ac57057f9e69639b3731227c22
Ben Hur <bhur777@gmail.com>
2023-08-27 17:05:57 -0400

    fix col-span

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -83,7 +83,7 @@ const ConversationPage = () => {
 							control={form.control}
 							name="prompt"
 							render={({ field }) => (
-								<FormItem className="col-span-12 lg:col-span-10">
+								<FormItem className="col-span-12 lg:col-span-6">
 									<FormControl className="m-0 p-0">
 										<Input
 											className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
