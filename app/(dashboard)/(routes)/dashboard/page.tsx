053d30b6af5ffed184672b9fa71969e9a0c2e78c
Ben Hur <bhur777@gmail.com>
2023-08-27 05:47:05 -0400

    Add router to push on click

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,7 +2,15 @@
 
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { ArrowRight, MessageSquare } from "lucide-react";
+import {
+	ArrowRight,
+	Code,
+	ImageIcon,
+	MessageSquare,
+	Music,
+	VideoIcon,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const tools = [
 	{
@@ -12,9 +20,39 @@ const tools = [
 		bgColor: "bg-violet-500/10",
 		href: "/conversation",
 	},
+	{
+		label: "Generate Image",
+		icon: ImageIcon,
+		color: "text-pink-500",
+		bgColor: "bg-pink-500/10",
+		href: "/image",
+	},
+	{
+		label: "Generate Music",
+		icon: Music,
+		color: "text-emerald-500",
+		bgColor: "bg-emerald-500/10",
+		href: "/music",
+	},
+	{
+		label: "Generate Video",
+		icon: VideoIcon,
+		color: "text-orange-500",
+		bgColor: "bg-orange-500/10",
+		href: "/video",
+	},
+	{
+		label: "Generate Code",
+		icon: Code,
+		color: "text-green-500",
+		bgColor: "bg-green-500/10",
+		href: "/code",
+	},
 ];
 
 const DashboardPage = () => {
+	const router = useRouter();
+
 	return (
 		<div>
 			<div className="mb-8 space-y-4">
@@ -28,6 +66,7 @@ const DashboardPage = () => {
 			<div className="px-4 md:px-20 lg:px-32 space-y-4">
 				{tools.map((tool) => (
 					<Card
+						onClick={() => router.push(tool.href)}
 						key={tool.href}
 						className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
 					>
