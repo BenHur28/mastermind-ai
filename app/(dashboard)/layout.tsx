9e6b1dc83c7f4ea2467456f10c85e6ed61e3d98d
Ben Hur <bhur777@gmail.com>
2023-08-27 04:34:17 -0400

    create and add navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,15 @@
+import Navbar from "@/components/navbar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="relative h-full">
-			<div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+			<div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
 				<div>Side Bar</div>
 			</div>
+			<main className="md:pl-72">
+				<Navbar />
+				{children}
+			</main>
 		</div>
 	);
 };
