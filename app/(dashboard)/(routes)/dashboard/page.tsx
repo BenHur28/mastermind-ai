ae1de96e899f01071d85c790674cbe12bc1629f6
Ben Hur <bhur777@gmail.com>
2023-08-27 05:41:21 -0400

    Finish tools card

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
-import { MessageSquare } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ArrowRight, MessageSquare } from "lucide-react";
 
 const tools = [
 	{
@@ -30,7 +31,13 @@ const DashboardPage = () => {
 						key={tool.href}
 						className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
 					>
-						{tool.label}
+						<div className="flex items-center gap-x-4">
+							<div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+								<tool.icon className={cn("w-8 h-8", tool.color)} />
+							</div>
+							<div className="font-semibold">{tool.label}</div>
+						</div>
+						<ArrowRight className="w-5 h-5" />
 					</Card>
 				))}
 			</div>
